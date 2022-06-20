c8e151e0cc5cc07d1ff25973f7c55f6c136eeffe
ZoricNikola00 <zoriclfc@gmail.com>
2022-06-21 01:47:37 +0200

    renaming functions

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams,useNavigate } from 'react-router-dom'
 import { useGlobalContext } from '../context'
 import { FaArrowLeft,FaArrowRight} from 'react-icons/fa';
